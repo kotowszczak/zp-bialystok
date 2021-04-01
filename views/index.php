abd3e656cb534fb9ed2f9b5d4e8d7dfe8ee4690e
jkotowski-emyad <75047775+jkotowski-emyad@users.noreply.github.com>
2021-04-02 01:04:21 +0200

    Ostylowanie strony glownej

--- views/index.php
+++ views/index.php
@@ -1,7 +1,10 @@
 <div class="blur">
     <div class="contents">
         <p>
-            <i class="icon">Ikonka Adresu</i>
+            <span class="material-icons rounded-circle">
+                mail_outline
+                <span class="sr-only">Ikonka Adresu</span>
+            </span>
 
             Zakład Poprawczy w Białymstoku<br />
             15-181 Białystok<br />
@@ -14,14 +17,23 @@
         </p>
 
         <p>
-            <i class="icon">Ikonka telefonu</i>
+            <span class="material-icons rounded-circle">
+                phone
+                <span class="sr-only">Ikonka telefonu</span>
+            </span>
+
+
 
             85 675 44 28, 85 675 30 59<br />
-            <span class="small-font">fax : 85 675 47 55</span>
+            <span class="fax">fax: 85 675 47 55</span>
         </p>
 
         <p>
-            <i class="icon">Ikonka małpy</i>
+            <span class="material-icons rounded-circle">
+                alternate_email
+                <span class="sr-only">Ikonka małpy</span>
+            </span>
+
 
             sekretariat@bialystok.zp.gov.pl
         </p>
